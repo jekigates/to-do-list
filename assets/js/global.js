5f567b25f08944eb7835145d89114475d4a8cf25
Jeki Gates <jekigates2004@gmail.com>
2022-09-23 19:32:16 +0700

    move some functions to global js

---
+++ assets/js/global.js
@@ -0,0 +1,58 @@
+function loadTodolists(type = "") {
+  table.innerHTML = "";
+  todolist.forEach(function (todo) {
+    if (todo.status === type) {
+      insertRow(todo.id, todo.name);
+    }
+  });
+}
+
+function deleteTodolists() {
+  Swal.fire({
+    title: "Are you sure?",
+    text: "You won't be able to revert this!",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete them!",
+  }).then((result) => {
+    if (result.isConfirmed) {
+      localStorage.removeItem("todolist");
+      Swal.fire("Deleted!", "Your todolists have been deleted.", "success");
+      todolist = [];
+      loadTodolists("notFinished");
+    }
+  });
+}
+
+function editTodolist(id) {
+  todoIndex = todolist.findIndex((todo) => todo.id === id);
+  todolist[todoIndex].name = document.getElementById(`input-todolist${id}`).value;
+  localStorage.setItem("todolist", JSON.stringify(todolist));
+}
+
+function insertRow(id = "", name = "", create = false) {
+  var row = table.insertRow();
+
+  if (create === true) {
+    id = todolist.length === 0 ? 0 : todolist[todolist.length - 1].id + 1;
+    todolist.push({ id: id, status: "notFinished", name: "" });
+    localStorage.setItem("todolist", JSON.stringify(todolist));
+  }
+
+  row.id = `todolist${id}`;
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  cell1.setAttribute("class", "text-center");
+  cell1.innerHTML = `<input type="checkbox" id="checkbox${id}" onclick="hideRow(${id})">`;
+  cell2.setAttribute("contenteditable", true);
+  cell2.innerHTML = `<input type="input" class="input-todolist" id="input-todolist${id}" value="${name}" oninput="editTodolist(${id})">`;
+}
+
+function hideRow(id) {
+  todoIndex = todolist.findIndex((todo) => todo.id === id);
+  todolist[todoIndex].status = "finished";
+  localStorage.setItem("todolist", JSON.stringify(todolist));
+  document.getElementById(`todolist${id}`).className = "d-none";
+}
