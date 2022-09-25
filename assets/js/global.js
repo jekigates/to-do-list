3cc8006c5d3433f271eb78b5d48f3449ebc789df
Jeki Gates <jekigates2004@gmail.com>
2022-09-25 13:52:09 +0700

    add delete button for each todolist

--- assets/js/global.js
+++ assets/js/global.js
@@ -7,7 +7,18 @@ function loadTodolists(type = "", checked = false) {
   });
 }
 
-function deleteTodolists() {
+function findTodolistIndex(id) {
+  return todolist.findIndex((todo) => todo.id === id);
+}
+
+function deleteTodolist(id) {
+  var todoIndex = findTodolistIndex(id);
+  todolist.splice(0, 1);
+  hideRow(id, false);
+  localStorage.setItem("todolist", JSON.stringify(todolist));
+}
+
+function deleteAllTodolists() {
   Swal.fire({
     title: "Are you sure?",
     text: "You won't be able to revert this!",
@@ -27,7 +38,7 @@ function deleteTodolists() {
 }
 
 function editTodolist(id) {
-  todoIndex = todolist.findIndex((todo) => todo.id === id);
+  var todoIndex = findTodolistIndex(id);
   todolist[todoIndex].name = document.getElementById(`input-todolist${id}`).value;
   localStorage.setItem("todolist", JSON.stringify(todolist));
 }
@@ -44,19 +55,41 @@ function insertRow(id = "", name = "", create = false, checked = false) {
   row.id = `todolist${id}`;
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
   cell1.setAttribute("class", "text-center");
-
   cell1.innerHTML =
     checked === false
-      ? `<input type="checkbox" id="checkbox${id}" onclick="hideRow(this, ${id})">`
-      : `<input type="checkbox" id="checkbox${id}" onclick="hideRow(this, ${id})" checked>`;
+      ? `<input type="checkbox" id="checkbox${id}" onclick="hideRow(${id})">`
+      : `<input type="checkbox" id="checkbox${id}" onclick="hideRow(${id})" checked>`;
   cell2.setAttribute("contenteditable", true);
   cell2.innerHTML = `<input type="input" class="input-todolist" id="input-todolist${id}" value="${name}" oninput="editTodolist(${id})">`;
+  cell3.className = "text-center";
+  cell3.innerHTML = `<button class="btn btn-outline-danger" onclick="deleteTodolist(${id})">X</button>`;
 }
 
-function hideRow(checkbox, id) {
-  todoIndex = todolist.findIndex((todo) => todo.id === id);
-  todolist[todoIndex].status = checkbox.checked === false ? "notFinished" : "finished";
-  localStorage.setItem("todolist", JSON.stringify(todolist));
+function hideRow(id, changeStatusOnly = true) {
+  if (changeStatusOnly) {
+    var checkbox = document.getElementById(`checkbox${id}`);
+    var todoIndex = findTodolistIndex(id);
+    todolist[todoIndex].status = checkbox.checked === false ? "notFinished" : "finished";
+    localStorage.setItem("todolist", JSON.stringify(todolist));
+  }
+
   document.getElementById(`todolist${id}`).className = "d-none";
+}
+
+function switchTodolist() {
+  var btnInsertRow = document.getElementById("btnInsertRow");
+  var inputSwitch = document.getElementById("inputSwitch");
+  var labelSwitch = document.getElementById("labelSwitch");
+
+  if (inputSwitch.checked) {
+    btnInsertRow.disabled = true;
+    labelSwitch.innerHTML = "Finished";
+    loadTodolists("finished", true);
+  } else {
+    btnInsertRow.disabled = false;
+    labelSwitch.innerHTML = "Not Finished";
+    loadTodolists("notFinished");
+  }
 }
